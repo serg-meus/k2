ed70bb4198a03466f783c7523f8d78d06d1f70f2
serg-meus <serg_meus@mail.ru>
2013-10-23 23:50:15 +0800

    Create engine.h

---
+++ engine.h
@@ -0,0 +1,59 @@
+#include "eval.h"
+#include "extern.h"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include "Timer.h"
+
+#define MAX_MOVES       256
+#define RESIGN_VALUE    750
+#define RESIGN_MOVES    3
+
+#define TUNE_PARAMETERS
+#define USE_PVS
+#define USE_NMR
+#define USE_FTL
+
+#ifdef TUNE_PARAMETERS
+#include <vector>
+#endif
+
+#ifdef CHECK_PREDICTED_VALUE
+//--------------------------------
+struct PredictedInfo
+{
+    Move    oppMove;
+    short   score;
+    short   depth;
+    short   state;
+};
+#endif // CHECK_PREDICTED_VALUE
+//--------------------------------
+
+void InitEngine();
+void Perft(int depth);
+short Search(int depth, short alpha, short beta);
+short Quiesce(short alpha, short beta);
+void StorePV(Move m);
+void UpdateStatistics(Move m, int dpt, unsigned i);
+short RootSearch(int depth, short alpha, short beta);
+void RootMoveGen(bool ic);
+void MainSearch();
+void InitSearch();
+void SearchResultOutput();
+void PlyOutput(short sc);
+void InitTime();
+bool ShowPV(int _ply);
+void Ambiguous(Move m);
+bool MakeLegalMove(char *mov);
+void EvalAllMaterialAndPST();
+bool FenToBoardAndVal(char *fen);
+bool DrawDetect();
+bool SimpleDrawByRepetition();
+void CheckForInterrupt();
+void MkMove(Move m);
+void UnMove(Move m);
+bool DrawByRepetitionInRoot(Move lastMove);
+void MakeNullMove();
+void UnMakeNullMove();
+bool NullMove(int depth, short beta, bool ic);
