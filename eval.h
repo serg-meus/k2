fb4dac14e03c89d0c760bf9d26296a734658ee46
serg-meus <serg_meus@mail.ru>
2013-10-23 23:51:00 +0800

    Create eval.h

---
+++ eval.h
@@ -0,0 +1,56 @@
+#include "movegen.h"
+
+#include <fstream>                  // для работы с файлами (ifstream, getline())
+#include <cstdlib>                  // для перевода строк в числа (atof())
+#include <iostream>
+
+#define USELESS_HALFMOVES_TO_DRAW 100
+#define MAXI(X, Y)       ((X) > (Y) ? (X) : (Y))
+
+//#define CHECK_PREDICTED_VALUE
+
+//#define EVAL_KING_TROPISM
+//--------------------------------
+//extern SC pst[6][2][8][8];
+//--------------------------------
+enum PriceList
+{
+    P_VAL_OPN   = 100,
+    N_VAL_OPN   = 395,
+    B_VAL_OPN   = 405,
+    R_VAL_OPN   = 600,
+    Q_VAL_OPN   = 1200,
+
+    P_VAL_END   = 125,
+    N_VAL_END   = 395,
+    B_VAL_END   = 405,
+    R_VAL_END   = 600,
+    Q_VAL_END   = 1200,
+
+    K_VAL       = 32000,
+    INF         = 32760,
+
+    SHIELD_K    = 30,
+    CLAMPED_R   = 250,
+//    OPP_NEAR_K  = 10,
+    UNSTOP_P    = 650,
+    DBL_PROMO_P = 65,
+    OPP_NEAR_K  = 20,
+
+};
+
+//--------------------------------
+
+
+//--------------------------------
+void InitEval();
+void FastEval(Move m);
+short Eval(/*short alpha, short beta*/);
+void EvalAllMaterialAndPST();
+void EvalPawns(bool stm);
+void SimpleKingShield(UC stm);
+void ClampedRook(UC stm);
+void SimpleKingNearOpp(UC stm);
+bool TestUnstoppable(int x, int y, UC stm);
+void SimpleKingDist(UC stm);
+short EvalAllKingDist(UC stm, UC king_coord);
