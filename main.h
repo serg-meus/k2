28ef8aa895105986d44a4cd7854ccd531184cadb
serg-meus <serg_meus@mail.ru>
2013-10-23 23:52:15 +0800

    Create main.h

---
+++ main.h
@@ -0,0 +1,61 @@
+#include "engine.h"
+#include <string>
+#include <cstdlib>
+
+//--------------------------------
+#ifdef NDEBUG
+    #define USE_THREAD_FOR_INPUT
+#endif
+
+#ifdef USE_THREAD_FOR_INPUT
+    #include <thread>
+#endif
+
+//--------------------------------
+extern double timeRemains;
+extern double timeBase;
+extern unsigned movesPerSession;
+extern double timeInc;
+extern unsigned timeMaxNodes;
+extern unsigned timeMaxPly;
+extern bool stop;
+extern bool _abort_;
+extern bool busy;
+extern UQ totalNodes;
+extern double totalTimeSpent;
+
+#ifdef TUNE_PARAMETERS
+extern std::vector <float> tuning_vec;
+#endif // TUNE_PARAMETERS
+
+
+//--------------------------------
+struct cmdStruct
+{
+    std::string command;
+    void (*foo)(std::string);
+};
+
+//--------------------------------
+int main(int argc, char* argv[]);
+bool CmdProcess(std::string in);
+bool LooksLikeMove(std::string in);
+void NewCommand(std::string in);
+void SetboardCommand(std::string in);
+void QuitCommand(std::string in);
+void PerftCommand(std::string in);
+void GoCommand(std::string in);
+void LevelCommand(std::string in);
+void ForceCommand(std::string in);
+void SetNodesCommand(std::string in);
+void SetTimeCommand(std::string in);
+void SetDepthCommand(std::string in);
+void Unsupported(std::string in);
+void GetFirstArg(std::string in, std::string *firstWord, std::string *remainingWords);
+void ProtoverCommand(std::string in);
+void StopEngine();
+void InterrogationCommand(std::string in);
+void ResultCommand(std::string in);
+void TimeCommand(std::string in);
+void EvalCommand(std::string in);
+void TestCommand(std::string in);
