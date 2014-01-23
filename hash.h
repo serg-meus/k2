2782176dc24f9c75d337fe0b06f319d49abf3690
serg-meus <serg_meus@mail.ru>
2014-01-23 23:32:55 +0700

    Update hash.h

--- hash.h
+++ hash.h
@@ -3,7 +3,11 @@
 #include <random>
 #include <map>
 
-#define MEN_TO_ZORB(X) ((X) < WHT ? (X) : ((X) - WHT + 5))
+//#define MEN_TO_ZORB(X) ((X) < WHT ? ((X) - 1) : ((X) - WHT + 5))
+//#define MEN_TO_ZORB(X) ((((X) & WHT) >> 2) - (((X) & WHT) >> 4) + (((X) & ~WHT) - 1))
+#define MEN_TO_ZORB(X) ((((((X) & WHT) - 1) >> 5) & 6) + ((X) & ~WHT) - 1)
+//#define MEN_TO_ZORB(X) men2zorb[X]
+//#define MEN_TO_ZORB(X) ((X) < WHT ? ((X) + 5) : ((X) - WHT - 1))
 
 bool    InitHashTable();
 UQ      InitHashKey();
