416fa56f922e5ebaee438fdde22bbcfac7dff7ce
serg-meus <serg_meus@mail.ru>
2014-07-24 10:40:50 +0800

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,14 +1,12 @@
 #include "chess.h"
 
 //--------------------------------
-UC  b[137],                                                             // board array in "0x88" style
-    men[64],                                                            // piece list
-    menNxt[64];                                                         // pointer to next piece
-    std::list<UC>   piece_list[2];
+UC  b[137];                                                             // board array in "0x88" style
+std::list<UC>   pcs[2];                                                 // list with piece coordinates and piece codes
 
 UC  attacks[240],                                                       // table for quick detect possible attacks
-    get_delta[240];                                                     // table for getting maximum ray length while verify possible attack
-SC  get_shift[240];                                                     // table for getting ray direction for verified attack
+    get_delta[240];                                                     // I'm already forget what's this
+SC  get_shift[240];                                                     // ... and this
 UC rays[7]   = {0, 8, 8, 4, 4, 8, 0};
 SC shifts[6][8] = {{ 0,  0,  0,  0,  0,  0,  0,  0},
                     { 1, 17, 16, 15, -1,-17,-16,-15},
@@ -51,14 +49,13 @@ void InitChess()
 //--------------------------------
 void InitBrd()
 {
-    UC pcs[] = {_R, _N, _B, _Q, _K, _B, _N, _R};
+    UC nums[] = {_R, _N, _B, _Q, _K, _B, _N, _R};
     UC nms[] = { 2,  6,  4,  1,  0,  5,  7,  3};
 
     memset(b, 0, sizeof(b));
-    memset(men, 0, sizeof(men));
-    memset(menNxt, 0, sizeof(menNxt));
-    piece_list[0].clear();
-    piece_list[1].clear();
+
+    pcs[0].clear();
+    pcs[1].clear();
 
     InitAttacks();
 
@@ -66,25 +63,15 @@ void InitBrd()
     for(i = 0; i < 8; i++)
     {
         b[XY2SQ(i, 1)]      = _P;
-        men[i + 0x29 ]      = XY2SQ(i, 1);
-        piece_list[1].push_back(XY2SQ(7 - i, 1));
+        pcs[1].push_back(XY2SQ(7 - i, 1));
         b[XY2SQ(i, 6)]     = _p;
-        men[i + 0x09 ]      = XY2SQ(i, 6);
-        piece_list[0].push_back(XY2SQ(7 - i, 6));
-        b[XY2SQ(i, 0)]      = pcs[i];
-        men[nms[i] + 0x21]  = XY2SQ(i, 0);
-        piece_list[1].push_front(XY2SQ(i, 0));
-        b[XY2SQ(i, 7)]      = pcs[i] ^ WHT;
-        men[nms[i] + 0x01]  = XY2SQ(i, 7);
-        piece_list[0].push_front(XY2SQ(i, 7));
+        pcs[0].push_back(XY2SQ(7 - i, 6));
+        b[XY2SQ(i, 0)]      = nums[i];
+        pcs[1].push_front(XY2SQ(i, 0));
+        b[XY2SQ(i, 7)]      = nums[i] ^ WHT;
+        pcs[0].push_front(XY2SQ(i, 7));
     }
-    menNxt[0x00]    = 0x10;
-    menNxt[0x20]    = 0x30;
-    for(i = 0; i < 0x10 - 1; i++)
-    {
-        menNxt[0x10 - i]    = 0x10 - i - 1;
-        menNxt[0x30 - i]    = 0x30 - i - 1;
-    }
+
     boardState[PREV_STATES + 0].capt    = 0;
     boardState[PREV_STATES + 0].cstl    = 0x0F;
     boardState[PREV_STATES + 0].ep      = 0;
@@ -133,57 +120,18 @@ void InitAttacks()
 bool BoardToMen()
 {
     unsigned wPieceCr = 0, bPieceCr = 0;
-    unsigned i, j;
-    for(i = 0; i < sizeof(men); i++)
-    {
-        men[i] = 0;
-        menNxt[i] = 0;
-    }
-    piece_list[0].clear();
-    piece_list[1].clear();
+    unsigned i;
+
+    pcs[0].clear();
+    pcs[1].clear();
     for(i = 0; i < sizeof(b); i++)
     {
         if(!ONBRD(i) || b[i] == __)
             continue;
-        if(b[i] < WHT)
-            men[1 + bPieceCr++] = i;
-        else
-            men[1 + WHT + wPieceCr++] = i;
-
-        piece_list[(b[i] & WHT) >> 5].push_front(i);
+        pcs[(b[i] & WHT) >> 5].push_front(i);
     }
-
-    UC tmp;
-    for(j = 0; j < bPieceCr; j++)
-        for(i = 0; i < bPieceCr - 1; i++)
-            if(streng[b[men[i + 1]]] < streng[b[men[i + 2]]])
-            {
-                tmp         = men[i + 1];
-                men[i + 1]      = men[i + 2];
-                men[i + 2]  = tmp;
-            }
-    for(j = WHT; j < WHT + wPieceCr; j++)
-        for(i = WHT; i < WHT + wPieceCr - 1; i++)
-            if(streng[(b[men[i + 1]] & ~WHT)] < streng[(b[men[i + 2]] & ~WHT)])
-            {
-                tmp         = men[i + 1];
-                men[i + 1]  = men[i + 2];
-                men[i + 2]  = tmp;
-            }
-
-//    menNxt[0x00]    = BLK + 1;
-//    menNxt[0x20]    = WHT + 1;
-    menNxt[0x00]    = bPieceCr;
-    menNxt[0x20]    = 0x20 + wPieceCr;
-    for(i = 0; i < bPieceCr - 1; i++)
-        menNxt[bPieceCr - i] = bPieceCr - i - 1;
-//        menNxt[i + 1] = i + 2;
-    for(i = 0; i < wPieceCr - 1; i++)
-        menNxt[0x20 + wPieceCr - i] = 0x20 + wPieceCr - i - 1;
-//        menNxt[0x20 + i + 1] = 0x20 + i + 2;
-
-    piece_list[0].sort(PieceListCompare);
-    piece_list[1].sort(PieceListCompare);
+    pcs[0].sort(PieceListCompare);
+    pcs[1].sort(PieceListCompare);
 
     return true;
 }
@@ -192,7 +140,7 @@ bool BoardToMen()
 bool FenToBoard(char *p)
 {
     char chars[] = "kqrbnpKQRBNP";
-    char pcs[]  = { _k, _q, _r, _b, _n, _p, _K, _Q, _R, _B, _N, _P};
+    char nums[]  = { _k, _q, _r, _b, _n, _p, _K, _Q, _R, _B, _N, _P};
     int mtr[]   = {0, 12, 6, 4, 4, 1, 0, 12, 6, 4, 4, 1};
     int i;
     material[0] = 0;
@@ -225,9 +173,9 @@ bool FenToBoard(char *p)
                     ;
                 if(i >= 12)
                     return false;
-                if((pcs[i] & 0x0F) == _p && (row == 0 || row == 7))
+                if((nums[i] & 0x0F) == _p && (row == 0 || row == 7))
                     return false;
-                b[XY2SQ(col, row)] = pcs[i];
+                b[XY2SQ(col, row)] = nums[i];
                 material[i >= 6]  += mtr[i];
                 pieces[i >= 6]++;
                 p++;
@@ -345,16 +293,16 @@ bool MakeCastle(Move m, UC fr)
         rFr += 0x70;
         rTo += 0x70;
     }
-    unsigned i;
-    for(i = wtm + 1; i < wtm + 0x20; i++)   //<< NB: wrong
-        if(men[i] == rFr)
-            break;
-    assert(men[i] == rFr);
 
-    boardState[PREV_STATES + ply].ncstl_r = i;
+    auto it = pcs[wtm >> 5].begin();
+    for(; it != pcs[wtm >> 5].end(); ++it)
+        if(*it == rFr)
+            break;
+
+    boardState[PREV_STATES + ply].cstl_rook_it = it;
     b[rTo] = b[rFr];
     b[rFr] = __;
-    men[i] = rTo;
+    *it = rTo;
 
     reversibleMoves = 0;
 
@@ -364,12 +312,12 @@ bool MakeCastle(Move m, UC fr)
 //--------------------------------
 void UnMakeCastle(Move m)
 {
-    UC rMen = boardState[PREV_STATES + ply].ncstl_r;
-    UC rFr = men[rMen];
+    auto r_it = boardState[PREV_STATES + ply].cstl_rook_it;
+    UC rFr = *r_it;
     UC rTo = rFr + (MOVEFLG(m) == mCS_K ? 2 : -3);
     b[rTo] = b[rFr];
     b[rFr] = __;
-    men[rMen] =rTo;
+    *r_it =rTo;
 }
 
 //--------------------------------
@@ -414,26 +362,24 @@ bool Attack(UC to, int xtm)
         return true;
 
     UC menNum = xtm;
-    for(int menCr = 0; menCr < pieces[xtm >> 5]; menCr++)
+    auto it = pcs[wtm >> 5].begin();
+    auto it_end = pcs[wtm >> 5].end();
+    for(;it != it_end; ++it)
     {
-        menNum  = menNxt[menNum];
-        fr      = men[menNum];
+        fr      = *it;
 
         int pt  = b[fr] & ~WHT;
         if(pt   == _p)
-//            break;
-            continue;
+            break;
 
-        assert((unsigned)(120 + to - fr) < sizeof(attacks)/sizeof(*attacks));
         UC att = attacks[120 + to - fr] & (1 << pt);
         if(!att)
             continue;
-        assert((unsigned)pt < sizeof(slider)/sizeof(*slider));
         if(!slider[pt])
             return true;
         if(SliderAttack(to, fr))
              return true;
-    }// for (menCr
+    }// for (; it
 
     return false;
 }
@@ -462,9 +408,13 @@ bool LegalSlider(UC fr, UC to, UC pt)
 bool Legal(Move m, bool ic)
 {
     if(ic || (b[MOVETO(m)] & ~WHT) == _k)
-        return !Attack(men[(wtm ^ WHT) + 1], wtm);
+    {
+        auto king = pcs[(wtm ^ WHT) >> 5].begin();
+        return !Attack(*king, wtm);
+    }
     UC fr = boardState[PREV_STATES + ply].fr;
-    UC to = men[(wtm ^ WHT) + 1];
+    auto king = pcs[(wtm ^ WHT) >> 5].begin();
+    UC to = *king;
     if(attacks[120 + to - fr] & (1 << _r))
         return LegalSlider(fr, to, _r);
     if(attacks[120 + to - fr] & (1 << _b))
@@ -562,5 +512,5 @@ void InitPawnStruct()
 //-----------------------------
 bool PieceListCompare(UC men1, UC men2)
 {
-    return streng[b[men1]] < streng[b[men2]];
+    return streng[b[men1] & ~WHT] < streng[b[men2] & ~WHT];
 }
