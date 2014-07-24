6f1e50ec6b328e419e6aa71de9a50dac3d49e45d
serg-meus <serg_meus@mail.ru>
2014-07-24 11:58:37 +0800

    Update movegen.h

--- movegen.h
+++ movegen.h
@@ -1,16 +1,21 @@
 #include "hash.h"
 #include "extern.h"
+#include <limits.h>
+//--------------------------------
+//#define DONT_USE_SEE_SORTING
+//#define DONT_USE_SEE_CUTOFF
 //--------------------------------
 #define LIGHT(X, s2m)   ((X) && (((X) & white) == (s2m)))
 #define DARK(X, s2m)    ((X) && (((X) & white) != (s2m)))
 
-#define MOVE_FROM_PV    121
-#define EQUAL_CAPTURE   119
-#define SECOND_KILLER   123
-#define FIRST_KILLER    125
-#define KING_CAPTURE    255
+
+#define EQUAL_CAPTURE   196
+#define MOVE_FROM_PV    197
+#define SECOND_KILLER   198
+#define FIRST_KILLER    199
 #define PV_FOLLOW       255
-#define BAD_CAPTURES    15
+#define KING_CAPTURE    255
+#define BAD_CAPTURES    63
 
 #define APPRICE_NONE    0
 #define APPRICE_CAPT    1
@@ -25,9 +30,7 @@ void    GenPawnCap(Move *list, int *movCr, short_list<UC, lst_sz>::iterator it);
 void    GenCastles(Move *list, int *movCr);
 int     GenCaptures(Move *list);
 void    AppriceMoves(Move *list, int moveCr, Move *best_move);
-void    Next(Move *m, int cur, int top, Move *ans);
 void    AppriceQuiesceMoves(Move *list, int moveCr);
 short   SEE(UC to, short frStreng, short val, bool stm);
 short_list<UC, lst_sz>::iterator SeeMinAttacker(UC to);
-void    AppriceHistory(Move *list, int moveCr);
 //--------------------------------
