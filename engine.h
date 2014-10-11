aac3cfd6c7a15d8ff87452153fde247e62131bd3
root <root@serg_meus@mail.ru>
2014-10-11 11:47:18 +0800

    Release of engine version 0.67

--- engine.h
+++ engine.h
@@ -5,7 +5,7 @@
 #include "Timer.h"
 
 //--------------------------------
-#define ENGINE_VERSION "066"
+#define ENGINE_VERSION "067"
 //--------------------------------
 //#define DONT_SHOW_STATISTICS
 //#define DONT_USE_FUTILITY
