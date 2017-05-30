2b22a6e07f64ac68986b332bd04156f2dc736d14
serg-meus <serg_meus@mail.ru>
2017-05-31 01:59:57 +0700

    Added test for attack tables

--- main.cpp
+++ main.cpp
@@ -17,15 +17,27 @@ int main(int argc, char* argv[])
     UNUSED(argc);
     UNUSED(argv);
 
+    size_t att_squares_w, att_squares_b, all_attacks_w, all_attacks_b;
     k2chess *chess = new k2chess();
-    auto att_squares_w = chess->test_count_attacked_squares(k2chess::white);
-    auto att_squares_b = chess->test_count_attacked_squares(k2chess::black);
-    auto all_attacks_w = chess->test_count_all_attacks(k2chess::white);
-    auto all_attacks_b = chess->test_count_all_attacks(k2chess::white);
+
+    att_squares_w = chess->test_count_attacked_squares(k2chess::white);
+    att_squares_b = chess->test_count_attacked_squares(k2chess::black);
+    all_attacks_w = chess->test_count_all_attacks(k2chess::white);
+    all_attacks_b = chess->test_count_all_attacks(k2chess::black);
     assert(att_squares_w == 18);
     assert(att_squares_b == 18);
     assert(all_attacks_w == 24);
     assert(all_attacks_b == 24);
+
+    assert(chess->FenToBoard((char *)"4k3/8/5n2/5n2/8/8/8/3RK3 w - - 0 1"));
+    att_squares_w = chess->test_count_attacked_squares(k2chess::white);
+    att_squares_b = chess->test_count_attacked_squares(k2chess::black);
+    all_attacks_w = chess->test_count_all_attacks(k2chess::white);
+    all_attacks_b = chess->test_count_all_attacks(k2chess::black);
+    assert(att_squares_w == 15);
+    assert(att_squares_b == 19);
+    assert(all_attacks_w == 16);
+    assert(all_attacks_b == 21);
 
     delete chess;
 }
