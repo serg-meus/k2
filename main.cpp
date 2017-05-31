2438e9e5083bd1272f5d545867f0ace8085a083b
serg-meus <serg_meus@mail.ru>
2017-05-31 12:50:19 +0700

    Refactoring test of attack tables

--- main.cpp
+++ main.cpp
@@ -10,6 +10,25 @@
 
 
 
+
+//--------------------------------
+void test_attack_tables(k2chess *chess, size_t att_w, size_t att_b,
+                        size_t all_w, size_t all_b)
+{
+    size_t att_squares_w, att_squares_b, all_attacks_w, all_attacks_b;
+    att_squares_w = chess->test_count_attacked_squares(k2chess::white);
+    att_squares_b = chess->test_count_attacked_squares(k2chess::black);
+    all_attacks_w = chess->test_count_all_attacks(k2chess::white);
+    all_attacks_b = chess->test_count_all_attacks(k2chess::black);
+    assert(att_squares_w == att_w);
+    assert(att_squares_b == att_b);
+    assert(all_attacks_w == all_w);
+    assert(all_attacks_b == all_b);
+}
+
+
+
+
 #define UNUSED(x) (void)(x)
 //--------------------------------
 int main(int argc, char* argv[])
@@ -17,27 +36,11 @@ int main(int argc, char* argv[])
     UNUSED(argc);
     UNUSED(argv);
 
-    size_t att_squares_w, att_squares_b, all_attacks_w, all_attacks_b;
     k2chess *chess = new k2chess();
-
-    att_squares_w = chess->test_count_attacked_squares(k2chess::white);
-    att_squares_b = chess->test_count_attacked_squares(k2chess::black);
-    all_attacks_w = chess->test_count_all_attacks(k2chess::white);
-    all_attacks_b = chess->test_count_all_attacks(k2chess::black);
-    assert(att_squares_w == 18);
-    assert(att_squares_b == 18);
-    assert(all_attacks_w == 24);
-    assert(all_attacks_b == 24);
+    test_attack_tables(chess, 18, 18, 24, 24);
 
     assert(chess->FenToBoard((char *)"4k3/8/5n2/5n2/8/8/8/3RK3 w - - 0 1"));
-    att_squares_w = chess->test_count_attacked_squares(k2chess::white);
-    att_squares_b = chess->test_count_attacked_squares(k2chess::black);
-    all_attacks_w = chess->test_count_all_attacks(k2chess::white);
-    all_attacks_b = chess->test_count_all_attacks(k2chess::black);
-    assert(att_squares_w == 15);
-    assert(att_squares_b == 19);
-    assert(all_attacks_w == 16);
-    assert(all_attacks_b == 21);
+    test_attack_tables(chess, 15, 19, 16, 21);
 
     delete chess;
 }
